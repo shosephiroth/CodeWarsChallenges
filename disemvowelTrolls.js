69510fb4acb074fd611b06ddc0f6c6bb3b5a5db0
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-30 08:50:12 -0400

    Update disemvowelTrolls.js
    This does not work but this was my thought process so far

--- disemvowelTrolls.js
+++ disemvowelTrolls.js
@@ -1,3 +1,10 @@
 function disemvowel(str) {
+  let vowels = [a, e, i, o, u];
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if str[i] !== vowels[i] {
+      newStr += vowels[i];
+    }
+  }
   return str;
 }
