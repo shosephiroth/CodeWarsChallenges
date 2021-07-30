9e95d7fd0947db1dc664fc21b62561a75bc6d460
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-30 10:46:00 -0400

    Update disemvowelTrolls.js
    still not complete and in the process of trying to fix it, I broke it again lol

--- disemvowelTrolls.js
+++ disemvowelTrolls.js
@@ -1,10 +1,13 @@
 function disemvowel(str) {
   let vowels = ['a', 'e', 'i', 'o', 'u'];
   let newStr = "";
+  let newStrArray =[];
   for (let i = 0; i < str.length; i++) {
     if (str[i] !== vowels[i]) {
-      newStr += vowels[i];
+      newStrArray.push(vowels[i]);
     }
   }
+  newStr = newStrArray.join(', ')
+  str = newStr;
   return str;
 }
