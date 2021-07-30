6ff1f71d9ef522274efc3efb4ba56eff4edda275
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-30 08:56:49 -0400

    Update disemvowelTrolls.js
    Got it! Just needed to add some syntax corrections. The items in the array were not entered (by myself) as strings and I forgot the parenthesis needed for the if statement.

--- disemvowelTrolls.js
+++ disemvowelTrolls.js
@@ -1,8 +1,8 @@
 function disemvowel(str) {
-  let vowels = [a, e, i, o, u];
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
   let newStr = "";
   for (let i = 0; i < str.length; i++) {
-    if str[i] !== vowels[i] {
+    if (str[i] !== vowels[i]) {
       newStr += vowels[i];
     }
   }
