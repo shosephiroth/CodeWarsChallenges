7b6bec06b3d6d8aed8c6dcf70b90b90e066646cc
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-31 01:00:54 -0400

    Update disemvowelTrolls.js
    Ok after reviewing the syntax, I realize I was using it incorrectly. I still don't quite have this but I'm printing just the vowels now..

--- disemvowelTrolls.js
+++ disemvowelTrolls.js
@@ -1,14 +1,11 @@
 function disemvowel(str) {
   let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O','U'];
-  let newStr = [];
-  for (let i = 0; i < str.length - 1; i++) {
-    if (str[i].includes(vowels[i])) {
-      newStr = newStr.push(str[i]);
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      newStr += (str[i]);
       console.log(newStr);
     }
   }
-  //str = newStr.join('');
-  //test
-  console.log(str);
   return str;
 }
