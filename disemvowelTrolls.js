f9991d6882a06cdaae8307dbe402466d97cc2015
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-31 01:05:46 -0400

    Update disemvowelTrolls.js
    To get it to print 'NOT' the vowels, why not throw a '!' to say 'if array, "vowels" does NOT include any letters in the current iteration of str[index], then add it to the string, "newStr"'. 5 tests passed but not completely done yet

--- disemvowelTrolls.js
+++ disemvowelTrolls.js
@@ -2,7 +2,7 @@ function disemvowel(str) {
   let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O','U'];
   let newStr = "";
   for (let i = 0; i < str.length; i++) {
-    if (vowels.includes(str[i])) {
+    if (!vowels.includes(str[i])) {
       newStr += (str[i]);
       console.log(newStr);
     }
