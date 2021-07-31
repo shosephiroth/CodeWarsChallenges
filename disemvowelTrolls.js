e63237389a4969a167ab1d5225f021d5aad86c60
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-31 00:50:14 -0400

    Update disemvowelTrolls.js
    still no go, I got an error stating that 'newStr.join()' is not a function; so I commented out to see what I could log

--- disemvowelTrolls.js
+++ disemvowelTrolls.js
@@ -3,10 +3,11 @@ function disemvowel(str) {
   let newStr = [];
   for (let i = 0; i < str.length - 1; i++) {
     if (str[i].includes(vowels[i])) {
-      newStr.push(str[i]);
+      newStr = newStr.push(str[i]);
+      console.log(newStr);
     }
   }
-  str = newStr.join('');
+  //str = newStr.join('');
   //test
   console.log(str);
   return str;
